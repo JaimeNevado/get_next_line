c10f630bb16e4407c345e884a169400efb69c11d
Jaime Nevado farfan <jnevado-@c2r9s2.42malaga.com>
2022-11-20 19:13:10 +0100

    algunos cambios

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jnevado- <jnevado-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 12:17:52 by jaimenevado       #+#    #+#             */
-/*   Updated: 2022/11/20 17:03:32 by jnevado-         ###   ########.fr       */
+/*   Updated: 2022/11/20 19:12:48 by jnevado-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ char	*ft_get_line(const char *buff)
 	{
 		line_lenght++;
 	}
+	if (buff[line_lenght] == '\n')
+		line_lenght++;
 	solution = (char *) malloc(sizeof(char) * (line_lenght + 1));
 	if (solution == NULL)
 		return (NULL);
@@ -33,6 +35,7 @@ char	*ft_get_line(const char *buff)
 		solution[i] = buff[i];
 		i++;
 	}
+	solution[i] = '\0';
 	return (solution);
 }
 
@@ -43,11 +46,13 @@ char	*ft_clean_buffer(char *buff)
 	int		i;
 	int		j;
 	char	*result;
-	
+
 	longitud_linea = 0;
 	longitud_resto = 0;
 
 	while (buff[longitud_linea] != 0 && buff[longitud_linea] != '\n')
+		longitud_linea++;
+	if (buff[longitud_linea] == '\n')
 		longitud_linea++;
 	while(buff[longitud_linea + longitud_resto])
 		longitud_resto++;
@@ -60,10 +65,10 @@ char	*ft_clean_buffer(char *buff)
 	j = longitud_linea + 1; // Comprobar que no se ha salido
 	while (buff[j] != '\0')
 	{
-		result[i] = buff[j];
-		j++;
+		result[i] = buff[j + i];
 		i++;
 	}
+	result[i] = '\0';
 	free (buff);
 	return (result);
 }
@@ -86,61 +91,64 @@ int	ft_check_line(char *buff)
 
 char	*get_next_line(int fd)
 {
-	static char	*resto = NULL;
+	static char	*resto;
 	char		*buff;
 	char		*linea;
 	int			leido;
 
-	if(BUFFER_SIZE < 1 || fd < 0)
+	if(BUFFER_SIZE < 1 || fd > 0)
 		return (NULL);
-	buff = (char *) malloc(BUFFER_SIZE + 1);
+	buff = (char *) malloc((BUFFER_SIZE +1) * sizeof(char));
 	if (buff == NULL)
 	{
-		free(resto);
-		resto = NULL;
+		if (resto)
+			free(resto);
 		return (NULL);
 	}
 	linea = NULL;
-	while (!ft_check_line(resto))
+	leido = 1;
+	if (!resto)
+	{
+		resto = (char *) malloc(sizeof(char));
+		resto[0] = '\0';
+	}
+	while ((!ft_check_line(resto)) && (leido > 0))
 	{
 		leido = read(fd, buff, BUFFER_SIZE);
+		buff[leido] = '\0';
 	//	printf("Bytes leídos - %d; Buffer - %s\n", leido, buff);
 		if (leido == -1)
 		{
 			free(buff);
 			free(resto);
-			resto = NULL;
 			return (NULL);
 		}
-		if (leido == 0)
-		{
-			if (resto == NULL)
-				resto = ft_strdup("");
-			break ;
-		}
-		resto = ft_strjoin(resto, buff);
+
+		if (resto || buff)
+			resto = ft_strjoin(resto, buff);
 		if (resto == NULL)
 		{
 			free(buff);
 			free(resto);
-			resto = NULL;
 			return (NULL);
 		}
 	}
 	linea = ft_get_line(resto);
 	resto = ft_clean_buffer(resto);
 	free(buff);
+	if (!linea || !resto)
+		return (NULL);
 //	printf("Resto = %s\n", resto);
 	return (linea);
 }
 
 
- int     main(void)
-{
-    int     fd;
-	
-    fd = open("text.txt",  O_RDONLY);
-	printf("\nRES - %s\n", get_next_line(fd));
-	printf("\nRES - %s\n", get_next_line(fd));
-	return (0);
-}
+//  int     main(void)
+// {
+//     int     fd;
+
+//     fd = open("text.txt",  O_RDONLY);
+// 	printf("%s\n", get_next_line(fd));
+// 	printf("%s\n", get_next_line(fd));
+// 	return (0);
+// }
